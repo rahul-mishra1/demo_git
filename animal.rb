99d3aeca28ecd2f4ffc47d58c13ede95d52df7d1
Naman <Naman@anchanto.com>
2021-06-07 13:47:31 +0530

    New dog function is introduced

--- animal.rb
+++ animal.rb
@@ -1,7 +1,15 @@
 class Animal
- def call
-  puts "Hello I am animal"
+ def dog
+  "Hello I am Dog"
  end
+
+ def call(name = nil)
+    if name == 'dog'
+      puts dog 
+    else
+      puts "Hello I am animal"
+    end
+  end
 end
 
-Animal.new.call
+Animal.new.call()
