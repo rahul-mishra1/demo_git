3e4f4ea6f7093ecdc56dadb949d922765da6012d
Naman <Naman@anchanto.com>
2021-06-07 14:00:10 +0530

    Added cat function

--- animal.rb
+++ animal.rb
@@ -1,15 +1,22 @@
 class Animal
+
+ def cat
+  "Hello I am Cat"
+ end
+
  def dog
   "Hello I am Dog"
  end
 
  def call(name = nil)
     if name == 'dog'
-      puts dog 
+      puts dog
+      elsif name == 'cat'
+      puts cat
     else
       puts "Hello I am animal"
     end
   end
 end
 
-Animal.new.call()
+Animal.new.call('cat')
